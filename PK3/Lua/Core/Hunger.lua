048e2ea5bb58e8b2cf7bd86821766f9756bc48d0
LJ Sonic <lamr@free.fr>
2024-10-01 18:25:55 +0200

    Show info bubble when hungry

--- PK3/Lua/Core/Hunger.lua
+++ PK3/Lua/Core/Hunger.lua
@@ -10,6 +10,45 @@ mod.MAX_HUNGER = 10*60*TICRATE
 
 
 ---@param player player_t
+local function updateInfoBubble(player)
+	local bubbles = player.itemapi_infoBubbles
+	local hunger = player.itemapi_hunger
+
+	local bubbleID
+	if hunger > mod.MAX_HUNGER / 4 then -- 25-100%
+		bubbleID = nil
+	elseif hunger > mod.MAX_HUNGER / 10 then -- 10-25%
+		bubbleID = "hungry"
+	elseif hunger > mod.MAX_HUNGER / 20 then -- 5-10%
+		bubbleID = "starving"
+	else -- 0-5%
+		bubbleID = "starving"
+	end
+
+	if bubbleID == "hungry" and not bubbles["hungry"] then
+		mod.startInfoBubble(player, {
+			id = "hungry",
+			sprite = SPR_IAPI,
+			frame = C,
+			scale = FU/2
+		})
+	elseif bubbleID ~= "hungry" and bubbles["hungry"] then
+		mod.stopInfoBubble(player, "hungry")
+	end
+
+	if bubbleID == "starving" and not bubbles["starving"] then
+		mod.startInfoBubble(player, {
+			id = "starving",
+			sprite = SPR_IAPI,
+			frame = D,
+			scale = FU/2
+		})
+	elseif bubbleID ~= "starving" and bubbles["starving"] then
+		mod.stopInfoBubble(player, "starving")
+	end
+end
+
+---@param player player_t
 function mod.eat(player, amount)
 	player.itemapi_hunger = min($ + amount, mod.MAX_HUNGER)
 end
@@ -37,4 +76,6 @@ function mod.updateHunger(player)
 	if newHunger == 0 and newHunger ~= oldHunger then
 		P_DamageMobj(player.mo, nil, nil, nil, DMG_INSTAKILL)
 	end
+
+	updateInfoBubble(player)
 end
